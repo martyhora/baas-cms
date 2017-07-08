044d53ab688947cc9415651df81572f960789e6e
Martin Hora <m.hora33@gmail.com>
2017-07-08 23:32:56 +0200

    Add type to parameterTypes

--- app/constants.ts
+++ app/constants.ts
@@ -1,15 +1,20 @@
-const apiBaseUrl = '/api'
-const apiVersion = '1'
-const apiBase = `${apiBaseUrl}/v${apiVersion}/`
+const apiBaseUrl = '/api';
+const apiVersion = '1';
+const apiBase = `${apiBaseUrl}/v${apiVersion}/`;
 
 export const apiUrl = {
     parameter: `${apiBase}parameters`,
     section: `${apiBase}sections`,
     content: `${apiBase}contents`
+};
+
+interface IParameterTypes {
+    'string': string;
+    'enum': string;
+    [key: string]: string;
 }
 
-
-export const parameterTypes = {
+export const parameterTypes: IParameterTypes = {
     'string': 'Text',
     'enum': 'Výčet položek'
-}
+};
